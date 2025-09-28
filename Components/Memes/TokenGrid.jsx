24e0763ff1f93ff18a24b998e3b844cc04e86e0a
stariik <tokokalandadze@gmail.com>
2025-09-28 15:26:45 +0400

    use client added

--- Components/Memes/TokenGrid.jsx
+++ Components/Memes/TokenGrid.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 // import TokenCard from "./TokenCard";
 import Lock from "./TokenCards/Lock";
--- app/portfolio/comps/StakingScroll.jsx
+++ app/portfolio/comps/StakingScroll.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React from 'react'
 import StakeCard from '../../../Components/Tokens/StakeCard'
 import { cardData } from "@/data/data";
