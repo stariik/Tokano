45b17245d1ac87db274bf1a43d3d9a5313a385fb
Lasha Andghuladze <l.andghuladze97@gmail.com>
2025-10-16 05:01:39 +0400

    feat: add walletconnect adapter.

--- Components/shared/layout-providers.tsx
+++ Components/shared/layout-providers.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useMemo } from "react";
-import { Adapter } from "@solana/wallet-adapter-base";
+import { Adapter, WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { LedgerWalletAdapter } from "@solana/wallet-adapter-ledger";
 import {
   ConnectionProvider,
@@ -11,13 +11,33 @@ import { RPC_ENDPOINT } from "@/lib/constants";
 import { ThemeProvider } from "@/hooks/useTheme";
 import TokanoSdkProvider from "@/contexts/tokano-sdk-context";
 import BalancesProvider from "@/contexts/balances-context";
+import { WalletConnectWalletAdapter } from "@walletconnect/solana-adapter";
 
 export default function LayoutProviders({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const wallets = useMemo<Adapter[]>(() => [new LedgerWalletAdapter()], []);
+  const wallets = useMemo<Adapter[]>(
+    () => [
+      new LedgerWalletAdapter(),
+      new WalletConnectWalletAdapter({
+        network: WalletAdapterNetwork.Mainnet,
+        options: {
+          relayUrl: "wss://relay.walletconnect.com",
+          // example WC app project ID
+          projectId: "e899c82be21d4acca2c8aec45e893598",
+          metadata: {
+            name: "Tokano App",
+            description: "Tokano App",
+            url: "https://github.com/anza-xyz/wallet-adapter",
+            icons: ["https://avatars.githubusercontent.com/u/35608259?s=200"],
+          },
+        },
+      }),
+    ],
+    [],
+  );
 
   return (
     <ConnectionProvider endpoint={RPC_ENDPOINT}>
