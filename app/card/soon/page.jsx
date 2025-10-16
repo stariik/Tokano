ea3b0d1e30448679eb003930dd23f9990e883daf
Lasha Andghuladze <l.andghuladze97@gmail.com>
2025-10-16 18:36:04 +0400

    chore: normalize line endings.

--- app/card/soon/page.jsx
+++ app/card/soon/page.jsx
@@ -6,15 +6,15 @@ import Soon from "./Soon";
 
 function page() {
   return (
-    <div className="lg:grid-cols-3 grid py-18 lg:py-6 md:px-6 2xl:px-2">
-      <div className="lg:col-span-2 w-full rounded-tr-4xl lg:grid gap-2 2xl:gap-8 lg:grid-cols-7">
+    <div className="grid py-18 md:px-6 lg:grid-cols-3 lg:py-6 2xl:px-2">
+      <div className="w-full gap-2 rounded-tr-4xl lg:col-span-2 lg:grid lg:grid-cols-7 2xl:gap-8">
         <div className="col-span-3">
           <TokenGrid
             gridCols="grid-cols-2 lg:grid-cols-1 xl:grid-cols-2"
             filterVariant="portfolio"
           />
         </div>
-        <div className="lg:col-span-4 mx-2 sm:mx-auto max-w-full sm:max-w-2/3 lg:max-w-full">
+        <div className="mx-2 max-w-full sm:mx-auto sm:max-w-2/3 lg:col-span-4 lg:max-w-full">
           <div className="">
             <Soon />
           </div>
