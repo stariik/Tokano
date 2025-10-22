57351a2fa2ce5e364818648cad2a0864682035b7
stariik <tokokalandadze@gmail.com>
2025-10-22 23:04:29 +0400

    stakenomics page layout done

--- app/stakenomics/page.jsx
+++ app/stakenomics/page.jsx
@@ -12,7 +12,7 @@ function page() {
   const selectedTokenData = getTokenById(selectedToken);
 
   return (
-    <div className="grid gap-2 md:px-2 lg:grid-cols-3 lg:py-6 2xl:gap-4 2xl:px-2">
+    <div className="mx-auto grid gap-4 sm:max-w-lg md:max-w-full md:grid-cols-2 md:px-2 lg:grid-cols-3 lg:gap-2 lg:py-6 2xl:gap-4 2xl:px-2">
       <div className="w-full gap-2 rounded-tr-4xl lg:col-span-2 lg:grid lg:grid-cols-10 2xl:gap-4">
         <div className="col-span-4 max-w-120">
           <TokenGrid
@@ -20,7 +20,7 @@ function page() {
             filterVariant="portfolio"
           />
         </div>
-        <div className="mx-2 max-w-full sm:mx-auto sm:max-w-2/3 md:mx-0 lg:col-span-6 lg:max-w-full flex justify-center md:block ">
+        <div className="mx-2 flex max-w-full flex-col justify-center sm:mx-auto md:mx-0 md:block lg:col-span-6 lg:max-w-full">
           <div className="">
             <CryptoWallet
               selectedToken={selectedToken}
